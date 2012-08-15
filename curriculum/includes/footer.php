918fcde3fddd758a6caf16c710d32e71c3174107
Phillip Conrad <pconrad@cs.ucsb.edu>
2012-08-15 11:28:54 -0700

    stubs for header and footer, and basis directory structure

---
+++ curriculum/includes/footer.php
@@ -0,0 +1,5 @@
+<!-- start: footer.php -->
+<div id="footer">
+<p>Footer for AT Curriculum website goes here</p>
+</div>
+<!-- end: footer.php -->
---
+++ curriculum/includes/header.php
@@ -0,0 +1,5 @@
+<!-- start: header.php -->
+<div id="header">
+<p>Header for AT Curriculum website goes here</p>
+</div>
+<!-- end: header.php -->
