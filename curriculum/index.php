c9338788d158eb0363bcda39a1eb0cafcbc55802
gdreschler <gdunit91@gmail.com>
2012-08-20 10:52:56 -0700

    adding h1 element, working on trying upstream pushes

--- curriculum/index.php
+++ curriculum/index.php
@@ -8,7 +8,9 @@
 <body>
 <div id="container">
  <?php require_once("includes/header.php"); ?>
-
+	<h1> Animal Tlatoque Curriculum </h1>
+	<p> Content goes here... </p>
+		
 
 
  <?php require_once("includes/footer.php"); ?>
