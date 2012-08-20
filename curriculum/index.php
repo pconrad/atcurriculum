5e83aecbd9036bc0c788766bd02e2f52b44ba9be
Phillip Conrad <pconrad@cs.ucsb.edu>
2012-08-20 10:36:25 -0700

    added root web page and basic css

---
+++ curriculum/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Animal Tlatoque Curriculum</title>
+  <link rel="stylesheet" href="css/atcurr.css" />
+</head>
+<body>
+<div id="container">
+ <?php require_once("includes/header.php"); ?>
+
+
+
+ <?php require_once("includes/footer.php"); ?>
+</div>
+</body>
+</html>
